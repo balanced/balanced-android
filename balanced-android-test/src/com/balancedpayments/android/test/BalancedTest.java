0566414e0721cee7850f5e2de645e9a99a39337f
Ben Mills <ben@unfiniti.com>
2013-07-18 20:27:07 -0600

    Test updates

--- balanced-android-test/src/com/balancedpayments/android/test/BalancedTest.java
+++ balanced-android-test/src/com/balancedpayments/android/test/BalancedTest.java
@@ -10,28 +10,43 @@ import com.balancedpayments.android.exception.CardDeclinedException;
 import com.balancedpayments.android.exception.CardNotValidatedException;
 
 public class BalancedTest extends AndroidTestCase {
-	private String marketplaceURI = "/v1/marketplaces/TEST-MP11mTuSoch2Eb1vrdwsSi2i";
-	
-	public void testTokenizeCard() {
-		String cardURI = "";
-		Context context = getContext();
-		
-		
-		Card card = new Card("4242424242424242", 9, 2014, "123");
-	      Balanced balanced = new Balanced(marketplaceURI, context);
-	      try {
-	          cardURI = balanced.tokenizeCard(card);
-	      }
-	      catch (CardNotValidatedException e) {
-	          
-	      }
-	      catch (CardDeclinedException e) {
-	          
-	      }
-	      catch (Exception e) {
-	          e.printStackTrace();
-	      }
-	      
-	      Log.d("BalancedTest", "********** CARD URI: " + cardURI);
-	}
+   private String marketplaceURI = "/v1/marketplaces/TEST-MP11mTuSoch2Eb1vrdwsSi2i";
+   
+   public void testTokenizeCard() {
+      String cardURI = "";
+
+      Card card = new Card("4242424242424242", 9, 2014, "123");
+      Balanced balanced = new Balanced(marketplaceURI, getContext());
+      try {
+         cardURI = balanced.tokenizeCard(card);
+      }
+      catch (CardNotValidatedException e) {}
+      catch (CardDeclinedException e) {}
+      catch (Exception e) {}
+
+      assertTrue(cardURI.contains("TEST-MP"));
+   }
+   
+   public void testTokenizeCardDeclined() {
+      String cardURI = "";
+      String error = "";
+      
+      Card card = new Card("4222222222222220", 9, 2014, "123");
+      
+      assertTrue(card.isValid());
+      
+      Balanced balanced = new Balanced(marketplaceURI, getContext());
+      try {
+         cardURI = balanced.tokenizeCard(card);
+      }
+      catch (CardNotValidatedException e) {
+         error = e.getMessage();
+      }
+      catch (CardDeclinedException e) {
+         error = e.getMessage();
+      }
+      catch (Exception e) {}
+      
+      assertTrue(error.contains("Customer call bank"));
+   }
 }
