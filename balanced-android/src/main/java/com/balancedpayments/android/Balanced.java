de44dc8dbf8404688c9354cb4adec9ae82c4b354
Ben Mills <ben@unfiniti.com>
2014-03-26 17:12:26 -0600

    Continue with 1.1-SNAPSHOT

--- balanced-android/src/main/java/com/balancedpayments/android/Balanced.java
+++ balanced-android/src/main/java/com/balancedpayments/android/Balanced.java
@@ -23,7 +23,7 @@ import java.util.Map;
  */
 public class Balanced
 {
-   public static final String VERSION = "1.0.1";
+   public static final String VERSION = "1.1-SNAPSHOT";
    private static String API_URL = "https://api.balancedpayments.com";
    private static String API_VERSION = "1.1";
    private static int connectionTimeout = 6000;
